dfa931ac2429c8811a8eb7b08e392b62f7221be6
arpita4878 <arpitaseth192@gmail.com>
2025-09-04 15:50:47 +0530

    first commit

---
+++ API/src/app.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+import { connectDB } from "./config/db.js"
+
+
+import userRouter from "./routes/user.router.js";
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/api/users", userRouter);
+
+
+
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+});
+
---
+++ API/src/models/user.model.js
@@ -0,0 +1,81 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: Number,
+    designation: { type: String, trim: true },
+    name: { type: String, required: [true, "Name is required"], trim: true },
+    surname: { type: String, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      sparse: true, // allow empty for supermarket customers
+      lowercase: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+      trim: true,
+      unique:true
+    },
+
+    
+    password: {
+      type: String,
+      required: function () {
+        return this.role === "online_customer"; 
+      },
+    },
+
+
+    role: {
+      type: String,
+      enum: [
+        "super_admin",
+        "branch_manager",
+        "staff",
+        "delivery_boy",
+        "supermarket_customer",
+        "online_customer",
+      ],
+      default: "supermarket_customer",
+    },
+
+    branch: {
+      type: String,
+      ref: "Branch",
+    },
+
+
+    birthday: { type: Date },
+    registeredDate: { type: Date, default: Date.now },
+    lastOrderDate: { type: Date },
+
+   
+    points: { type: Number, default: 0 },
+    orderCount: { type: Number, default: 0 },
+    totalOrderAmount: { type: Number, default: 0 },
+
+    nationality: { type: String, trim: true },
+
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.getResetPasswordToken = function () {
+ ;
+
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.resetPasswordExpire = Date.now() + 15 * 60 * 1000;
+  return resetToken;
+};
+
+export default mongoose.model("Users", userSchema);
