b2978b3fd6bd608eace8745c5038de2a48edbbc2
karishma <karishma>
2025-09-11 18:22:31 +0530

    service-feedbacks improved

--- API/src/models/serviceFeedback.model.js
+++ API/src/models/serviceFeedback.model.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose";
 
 const serviceFeedbackSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  branch: { type: String, required: true },
-  comment: { type: String, required: true },
-  reasonOfFeedback: { type: String, required: true }, // reason field
-  serviceRating: { type: Number, min: 1, max: 5, required: true },
-  quantityRating: { type: Number, min: 1, max: 5, required: true },
-  deliveryRating: { type: Number, min: 1, max: 5, required: true },
-  date: { type: Date, default: Date.now },
-  orderId: { type: String, required: true },
-//   viewFile: { type: String }, // file ka path/url store hoga (image, pdf, etc.)
-  status: { type: String, enum: ["Pending", "In Progress", "Resolved"], default: "Pending" }
-}, { timestamps: true });
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    branch: { type: mongoose.Schema.Types.ObjectId, ref: "Branch", required: true },
+    comment: { type: String },
+    reasonOfFeedback: { type: String },
+    serviceRating: { type: Number, min: 1, max: 5 },
+    quantityRating: { type: Number, min: 1, max: 5 },
+    deliveryRating: { type: Number, min: 1, max: 5 },
+    date: { type: Date, default: Date.now },
+    orderId: { type: mongoose.Schema.Types.ObjectId, ref: "Order" },
+    status: { type: String, enum: ["pending", "accepted", "dispatched", "delivered"], default: "pending" }
+});
 
 export default mongoose.model("ServiceFeedback", serviceFeedbackSchema);
