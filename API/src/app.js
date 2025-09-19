3b3237e6ab31cead5f83cb99ee78470eb37b3f36
arpita4878 <arpitaseth192@gmail.com>
2025-09-19 11:03:53 +0530

    "delete duplicate"

--- API/src/app.js
+++ API/src/app.js
@@ -37,6 +37,7 @@ const port = process.env.PORT || 5000;
 const httpServer = createServer(app);
 
 // Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
