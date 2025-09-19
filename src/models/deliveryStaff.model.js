ad41d58aa03e86711aba74b56a67e2d31fde51be
arpita4878 <arpitaseth192@gmail.com>
2025-09-19 11:04:15 +0530

    "delete duplicate"

--- src/models/deliveryStaff.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const deliveryStaffSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  surname: { type: String, required: true },
-  phone: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model("DeliveryStaff", deliveryStaffSchema);
